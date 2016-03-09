fb303ced41b939201e09d79e095e9e6ce9da0b6c
Mateusz Gawliński <matewka@gmail.com>
2016-03-09 20:18:43 +0100

    Improvements in config.

--- src/helpers/config.js
+++ src/helpers/config.js
@@ -1,32 +1,79 @@
 'use strict';
 
-const appRootPath = require('app-root-path');
-const fs = require('fs');
+import defaultCssConverter from '../converters/css/default.css.converter.js';
+import defaultHtmlConverter from '../converters/html/default.html.converter';
+import defaultJsConverter from '../converters/js/default.js.converter';
 
-let config = {};
+const config = {};
 
-try {
-  const configRaw = fs.readFileSync(appRootPath + '/.uglicssyrc');
-  const configString = configRaw.toString();
+(() => {
+  const appRootPath = require('app-root-path');
+  const fs = require('fs');
+
   let configRc;
 
-  if (configString) {
-    configRc = JSON.parse(configString);
+  function isFunction(variable) {
+    return typeof variable === 'function';
   }
 
-  if (configRc && Array.isArray(configRc.presets)) {
-    config.presetsConf = configRc.presets.reduce((conf, preset) => {
-      conf.push(require(preset));
-    }, []);
+  function populatePresets() {
+    const converters = {
+      css: [defaultCssConverter],
+      html: [defaultHtmlConverter],
+      js: [defaultJsConverter]
+    };
+
+    if (configRc && configRc.presets) {
+      if (!Array.isArray(configRc.presets)) {
+        configRc.presets = [configRc.presets];
+      }
+
+      configRc.presets.forEach((presetName) => {
+        if (!presetName) {
+          return;
+        }
+
+        let preset = require(presetName);
+
+        function addConverters(type) {
+          if (Array.isArray(preset.converters[type])) {
+            preset.converters[type].forEach((converter) => converters[type].push(converter));
+          } else if (isFunction(preset.converters[type])) {
+            converters[type].push(preset.converters[type]);
+          }
+        }
+
+        if (preset && preset.converters) {
+          addConverters('css');
+          addConverters('html');
+          addConverters('js');
+        } else {
+          console.error('Uglicssy preset: ' + presetName + ' doesn\'t have the \'converters\' property.');
+        }
+      });
+    }
+
+    return converters;
   }
-} catch (err) {
-  if (err.errno === -2) {
-    console.warn('Configuration file .uglicssyrc not found.');
-  } else if (err.code === 'MODULE_NOT_FOUND') {
-    console.error('Error during presets loading:', err);
-  } else {
-    console.error('Could not parse the .uglicssyrc configuration file due to the following error:', err);
+
+  try {
+    const configRaw = fs.readFileSync(appRootPath + '/.uglicssyrc');
+    const configString = configRaw.toString();
+
+    if (configString) {
+      configRc = JSON.parse(configString);
+    }
+  } catch (err) {
+    if (err.errno === -2) {
+      console.warn('Configuration file .uglicssyrc not found.');
+    } else if (err.code === 'MODULE_NOT_FOUND') {
+      console.error('Error during presets loading:', err);
+    } else {
+      console.error('Could not parse the .uglicssyrc configuration file due to the following error:', err);
+    }
   }
-}
+
+  config.converters = populatePresets();
+})();
 
 export default config;
--- src/index.js
+++ src/index.js
@@ -2,40 +2,15 @@
 
 import Classes from './classes/classes.class';
 
-import defaultCssConverter from './converters/css/default.css.converter.js';
-import defaultHtmlConverter from './converters/html/default.html.converter';
-import defaultJsConverter from './converters/js/default.js.converter';
-
 import cssProcessor from './helpers/processors/css.processor';
 import htmlProcessor from './helpers/processors/html.processor';
 import jsProcessor from './helpers/processors/js.processor';
 
 import config from './helpers/config';
 
-const converters = (() => {
-
-  const conv = {
-    css: [defaultCssConverter],
-    html: [defaultHtmlConverter],
-    js: [defaultJsConverter]
-  };
-
-  if (config && config.presetsConf) {
-    config.presetsConf.forEach((conf) => {
-      conv[conf.type].push(conf.converter);
-    });
-  }
-
-  return conv;
-})();
-
 module.exports = class Uglicssy {
   constructor() {
     this.classes = new Classes();
-  }
-
-  static addConverter(converter, type) {
-    converters[type].push(converter);
   }
 
   static bundle() {
@@ -44,15 +19,15 @@ module.exports = class Uglicssy {
 
   convert(contents, type) {
     if (type === 'css') {
-      return cssProcessor(contents, this.classes, converters.css);
+      return cssProcessor(contents, this.classes, config.converters.css);
     }
 
     if (type === 'html') {
-      return htmlProcessor(contents, this.classes, converters.html);
+      return htmlProcessor(contents, this.classes, config.converters.html);
     }
 
     if (type === 'js') {
-      return jsProcessor(contents, this.classes, converters.js);
+      return jsProcessor(contents, this.classes, config.converters.js);
     }
   }
 };
