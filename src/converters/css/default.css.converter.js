978ac3abf361db94cc41c504147c307ebc3fe0b6
Mateusz Gawliński <matewka@gmail.com>
2016-03-12 17:12:49 +0100

    Improved CSS converter.

--- src/converters/css/default.css.converter.js
+++ src/converters/css/default.css.converter.js
@@ -1,10 +1,10 @@
 'use strict';
 
-const cssClassRegex = /^[a-zA-Z0-9_-]+/;
+const cssClassRegex = /\.[a-zA-Z0-9_-]+/;
 
 export default function defaultCssConverter(selector, classes, minifyFn) {
-  if (selector.indexOf('.') === 0) {
-    return '.' + selector.substr(1).replace(cssClassRegex, (className) => {
+  if (selector.indexOf('.') !== -1) {
+    return selector.replace(cssClassRegex, (className) => {
       return minifyFn(className, classes);
     });
   }
